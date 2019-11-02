5a93c704d38fc73fc9419e43f122a51b8ac0decb
theindianappguy <theindianappguy@gmail.com>
2019-11-02 22:26:20 +0530

    Initial commit

---
+++ app/src/main/java/com/theindianappguy/androidxmlloginui5/MainActivity.java
@@ -0,0 +1,39 @@
+package com.theindianappguy.androidxmlloginui5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Making notification bar transparent
+        if (Build.VERSION.SDK_INT >= 21) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+        setContentView(R.layout.activity_main);
+
+        // making notification bar transparent
+        changeStatusBarColor();
+
+        /*font credit: https://www.fontsquirrel.com/fonts/raleway*/
+    }
+
+    /**
+     * Making notification bar transparent
+     */
+    private void changeStatusBarColor() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+}
